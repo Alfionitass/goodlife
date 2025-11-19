bbf9d215ce0dbb2ab7967fce323993f8abe923dc
Alfionitas <>
2025-11-19 15:00:17 +0700

    1. added products prom to NavBar 2. added comment screens material tailwind in tailwind config 3. update NavBar component to add entry point for products in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { AppRoutes } from "./Routes";
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 import data from "./data/data.json";
+import { products } from "./data/product";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <NavBar menu={data.menu} logo={data.logo.goodlife} />
+      <NavBar menu={data.menu} logo={data.logo.goodlife} products={products} />
       <div className="grow">
         <AppRoutes />
       </div>
